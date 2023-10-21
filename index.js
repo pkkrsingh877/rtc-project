b02bc8db0862a152cc1bc61cb1d71a9181203b8d
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-10-21 10:42:21 +0530

    Added Ejs and Ejs-Mate

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+//setting up ejs
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
 
 app.listen(8000, () => {
     console.log("The app is listening at port 8000.");
